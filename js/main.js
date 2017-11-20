4d63e5b25f8625896f457040a0ee29270442a42e
hn <mofmofsyrup@gmail.com>
2017-11-20 12:01:40 +0900

    Make start button works, and Make timer stop when if remaining time is less than 0sec

--- js/main.js
+++ js/main.js
@@ -10,18 +10,44 @@
     var startTime;
     var timeLeft;
     var timeToCountDown = 4 * 1000;
+    var timerId;
+
+    // 分・秒に変換
+    function updateTimer(t) {
+        var d = new Date(t);
+        var m = d.getMinutes();
+        var s = d.getSeconds();
+        var ms = d.getMilliseconds();
+
+        // 桁数が足りないときは0で埋める
+        m = ('0' + m).slice(-2);
+        s = ('0' + s).slice(-2);
+        ms = ('00' + ms).slice(-3); // ミリ秒の場合は3桁
+
+        // 表示
+        timer.textContent = m + ':' + s + '.' + ms;
+    }
+
+    // カウントダウン
     function countDown() {
-        setTimeout(function() {
-            var elapsedTime = Date.now() - startTime;
-            timeLeft = timeToCountDown -
-            timeLeft = timeToCountDown - (Date.now() - startTime));
+        timerId = setTimeout(function() {
+            timeLeft = timeToCountDown - (Date.now() - startTime);
+            // 残り時間が0以下になったらタイマーを止める
+            if(timeLeft < 0) {
+                clearTimeout(timerId);
+                timeLeft = 0;
+                updateTimer(timeLeft);
+                return;
+            }
+            updateTimer(timeLeft);
+            countDown();
         }, 10);
     }
 
+    // スタートボタン
     start.addEventListener('click', function() {
         // 開始時刻
         startTime = Date.now();
-
         countDown();
     });
-})
+})();
